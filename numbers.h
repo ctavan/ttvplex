2cd80b26f20f7aa9a1afa8ed37c4cfcb3b3eeaae
Christoph Tavan <tavan@nemata.com>
2011-01-11 14:36:12 +0100

    merged mpf_class branch

---
+++ numbers.h
@@ -0,0 +1,10 @@
+#ifndef numbers_h
+#define numbers_h
+
+// GMP: The GNU Multiple Precision Arithmetic Library
+#include <gmpxx.h>
+
+typedef mpq_class my_rational;
+typedef mpf_class my_float;
+
+#endif
