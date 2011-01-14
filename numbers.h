7098cb4a874aa8d7f3907c15e6792b7d4c9dd2a6
Christoph Tavan <tavan@nemata.com>
2011-01-14 19:33:14 +0100

    typo

--- numbers.h
+++ numbers.h
@@ -4,7 +4,7 @@
 // GMP: The GNU Multiple Precision Arithmetic Library
 #include <gmpxx.h>
 
-typedef mpq_class my_rational;	//!< Redefine variable type for exact rartional numbers
+typedef mpq_class my_rational;	//!< Redefine variable type for exact rational numbers
 typedef mpf_class my_float;		//!< Redefine variable type for arbitrary precision floats
 
 #endif
